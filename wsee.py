b4ab22de7007d89c87bab477ccf0a0b44d8bc728
EMSI <62210133+MC874@users.noreply.github.com>
2022-05-19 10:31:18 +0700

    chore: Code Cleanup
    - Separate codes into per function
    - Variable now casted as class
    - Simulate `goto` jump for cleaner code
    - After scanning is done, its bring user back to `menu()`
    - Added user `input()` to avoid auto close, casted by any input

--- wsee.py
+++ wsee.py
@@ -16,88 +16,68 @@ class colors:
 	RED_BG = '\033[41m\033[1m'
 	GREEN_BG = '\033[42m'
 
-os.chdir(dirname(abspath(__file__)))
+class inf:
+	expected_response = 101
+	control_domain = 'd2f99r5bkcyeqq.cloudfront.net'
+	headers = { "Host": inf.control_domain, "Upgrade": "websocket", "DNT":  "1", "Accept-Language": "*", "Accept": "*/*", "Accept-Encoding": "*", "Connection": "keep-alive, upgrade", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36" }
+	file_hosts = ""
+	result_success = []
+	num_file = 1
+	columns = defaultdict(list)
+	txtfiles= []
+	hostpath = 'host'
 
-print('''
-
-__  _  ________ ____   ____  
-\ \/ \/ /  ___// __ \_/ __ \ 
- \     /\___ \\  ___/\  ___/ 
-  \/\_//____  >\___  >\___  >
-            \/     \/     \/                       
-                               
-''')
-print("    [" + colors.RED_BG + " Domain : Fronting " + colors.ENDC + "]")
-print("     ["+colors.RED_BG+" Author " + colors.ENDC + ":" + colors.GREEN_BG + " Kiynox " + colors.ENDC + "]")
-print("")
-
-print(" 1. Scanning File List from .txt")
-print(" 2. Scanning File List from .csv")
-print(" 3. Scanning from Subdomain Enum [HackerTarget]")
-print(" 4. Local Websocket Finder")
-print(" Q to Quit")
-opsi=input(" Choose Option :  ")
-
-expected_response = 101
-control_domain = 'd2f99r5bkcyeqq.cloudfront.net'
-headers = { "Host": control_domain, "Upgrade": "websocket", "DNT":  "1", "Accept-Language": "*", "Accept": "*/*", "Accept-Encoding": "*", "Connection": "keep-alive, upgrade", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36" }
-file_hosts = ""
-result_success = []
-num_file = 1
-columns = defaultdict(list)
-
-txtfiles= []
-hostpath = 'host'
-if not os.path.exists(hostpath):
-	os.makedirs(hostpath)
-
-if str(opsi) == "1":
-	files = os.listdir(hostpath)
+def text():
+	global domainlist, parseddom
+	files = os.listdir(inf.hostpath)
 	for f in files:
 		if fnmatch.fnmatch(f, '*.txt'):
-			print( str(num_file),str(f))
-			num_file=num_file+1
-			txtfiles.append(str(f))
+			print( str(inf.num_file),str(f))
+			inf.num_file=inf.num_file+1
+			inf.txtfiles.append(str(f))
 
 	fileselector = input("Choose Target Files : ")
-	print("Target Chosen : " + txtfiles[int(fileselector)-1])
-	file_hosts = str(hostpath) +"/"+ str(txtfiles[int(fileselector)-1])
+	print("Target Chosen : " + inf.txtfiles[int(fileselector)-1])
+	inf.file_hosts = str(inf.hostpath) +"/"+ str(inf.txtfiles[int(fileselector)-1])
 
-	with open(file_hosts) as f:
+	with open(inf.file_hosts) as f:
 		parseddom = f.read().split()
 		
 	domainlist = list(set(parseddom))
 	domainlist = list(filter(None, parseddom))
+	engine()
 
-elif str(opsi) == "2":
-	files = os.listdir(hostpath)
+def csv():
+	global domainlist, parseddom
+	files = os.listdir(inf.hostpath)
 	for f in files:
 		if fnmatch.fnmatch(f, '*.csv'):
-			print( str(num_file),str(f))
-			num_file=num_file+1
-			txtfiles.append(str(f))
+			print( str(inf.num_file),str(f))
+			inf.num_file=inf.num_file+1
+			inf.txtfiles.append(str(f))
 	
 	fileselector = input("Select File : ")
-	print("File Selected : " + txtfiles[int(fileselector)-1])
-	file_hosts = str(hostpath) +"/"+ str(txtfiles[int(fileselector)-1])
+	print("File Selected : " + inf.txtfiles[int(fileselector)-1])
+	inf.file_hosts = str(inf.hostpath) +"/"+ str(inf.txtfiles[int(fileselector)-1])
 	
-	with open(file_hosts,'r') as csv_file:
+	with open(inf.file_hosts,'r') as csv_file:
 		reader = csv.reader(csv_file)
 	
 		for row in reader:
 			for (i,v) in enumerate(row):
-				columns[i].append(v)
-	parseddom=columns[9]+columns[3]
+				inf.columns[i].append(v)
+	parseddom=inf.columns[9]+inf.columns[3]
 	domainlist = list(set(parseddom))
 	domainlist = list(filter(None, parseddom))
+	engine()
 
-elif str(opsi) == "3":
+def enum():
 	subd = input("\nInput Domain: ")
 	subd = subd.replace("https://","").replace("http://","")
 	r = requests.get("https://api.hackertarget.com/hostsearch/?q=" + subd, allow_redirects=False)
 	yn = input("\nContinue Scanning? (y/n): ")
 	if yn.lower() == "y":
-		head = { "Host": control_domain, "Upgrade": "websocket", "DNT":  "1", "Accept-Language": "*", "Accept": "*/*", "Accept-Encoding": "*", "Connection": "keep-alive, upgrade", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36" }
+		head = { "Host": inf.control_domain, "Upgrade": "websocket", "DNT":  "1", "Accept-Language": "*", "Accept": "*/*", "Accept-Encoding": "*", "Connection": "keep-alive, upgrade", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36" }
 		sukses = []
 		if r.text == "error invalid host":
 			exit("ERR: error invalid host")
@@ -106,7 +86,7 @@ elif str(opsi) == "3":
 			subdo = re.findall("(.*?),",r.text)
 			for sub in subdo:
 				try:
-					req = requests.get(f"http://{sub}",headers=head,timeout=0.7,allow_redirects=False)
+					req = requests.get(f"http://{sub}",inf.headers=head,timeout=0.7,allow_redirects=False)
 					if req.status_code == 101:
 						print(" ["+colors.GREEN_BG+" HIT "+colors.ENDC+"] " + str(sub))
 						sukses.append(str(sub))
@@ -130,21 +110,21 @@ elif str(opsi) == "3":
 	else:
 		exit()
 
-elif str(opsi) == "4":
+elif wsocket():
 	wsocket = { "Connection": "Upgrade", "Sec-Websocket-Key": "dXP3jD9Ipw0B2EmWrMDTEw==", "Sec-Websocket-Version": "13", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36", "Upgrade": "websocket" }
 
-files = os.listdir(hostpath)
+	files = os.listdir(inf.hostpath)
 	for f in files:
 		if fnmatch.fnmatch(f, '*.txt'):
-			print( str(num_file),str(f))
-			num_file=num_file+1
-			txtfiles.append(str(f))
+			print( str(inf.num_file),str(f))
+			inf.num_file=inf.num_file+1
+			inf.txtfiles.append(str(f))
 
 	fileselector = input("Choose Target Files : ")
-	print("Target Chosen : " + txtfiles[int(fileselector)-1])
-	file_hosts = str(hostpath) +"/"+ str(txtfiles[int(fileselector)-1])
+	print("Target Chosen : " + inf.txtfiles[int(fileselector)-1])
+	inf.file_hosts = str(inf.hostpath) +"/"+ str(inf.txtfiles[int(fileselector)-1])
 
-	with open(file_hosts) as f:
+	with open(inf.file_hosts) as f:
 		parseddom = f.read().split()
 
 	domainlist = list(set(parseddom))
@@ -156,12 +136,12 @@ files = os.listdir(hostpath)
 
 	for domain in domainlist:
 			try:
-				r = requests.get("http://" + domain, headers=wsocket, timeout=1, allow_redirects=False)
-				if r.status_code == expected_response:
+				r = requests.get("http://" + domain, inf.headers=wsocket, timeout=1, allow_redirects=False)
+				if r.status_code == inf.expected_response:
 					print(" ["+colors.GREEN_BG+" HIT "+colors.ENDC+"] " + domain)
 					print(domain, file=open("RelateWebsocket.txt", "a"))
-					result_success.append(str(domain))
-				elif r.status_code != expected_response:
+					inf.result_success.append(str(domain))
+				elif r.status_code != inf.expected_response:
 					print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" +colors.RED_BG+" " + str(r.status_code) + " "+colors.ENDC+"]")
 			except (Timeout, ReadTimeout, ConnectionError):
 				print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " ["+ colors.RED_BG+" TIMEOUT "+colors.ENDC+"]")
@@ -173,41 +153,79 @@ files = os.listdir(hostpath)
 			except:
 				pass
 	
-	print(" Loaded : "  + colors.GREEN + str(len(result_success)) + colors.ENDC)
-	if len(result_success) >= 0:
+	print(" Loaded : "  + colors.GREEN + str(len(inf.result_success)) + colors.ENDC)
+	if len(inf.result_success) >= 0:
 		print(" Successfull Result : ")
-	for result in result_success:
+	for result in inf.result_success:
 		print(colors.GREEN + "  " + result + colors.ENDC)
+	input("Continue..")
+	menu()
 
-else:
-	exit()
+def engine():
+	print(" Loaded: " + colors.GREEN + str(len(domainlist)) + colors.ENDC + " Total of Unique Host: " + str(len(parseddom)) + " host")
+	print("")
+	input(colors.GREEN + "[ENTER] Start Scan ....." + colors.ENDC)
+	print("")
+	
+	for domain in domainlist:
+			try:
+				r = requests.get("http://" + domain, inf.headers=inf.headers, timeout=0.7, allow_redirects=False)
+				if r.status_code == inf.expected_response:
+					print(" ["+colors.GREEN_BG+" HIT "+colors.ENDC+"] " + domain)
+					print(domain, file=open("RelateCFront.txt", "a"))
+					inf.result_success.append(str(domain))
+				elif r.status_code != inf.expected_response:
+					print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" +colors.RED_BG+" " + str(r.status_code) + " "+colors.ENDC+"]")
+			except (Timeout, ReadTimeout, ConnectionError):
+				print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" + colors.RED_BG +" TIMEOUT "+colors.ENDC+"]")
+			except(ChunkedEncodingError, ProtocolError, InvalidChunkLength):
+				print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" + colors.RED_BG+" Invalid Length "+colors.ENDC + "]")
+				pass
+			except(TooManyRedirects):
+				print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" +colors.RED_BG+" Redirects Loop "+colors.ENDC+"]")
+			except:
+				pass
+	
+	print(" Loaded : "  + colors.GREEN + str(len(inf.result_success)) + colors.ENDC)
+	if len(inf.result_success) >= 0:
+		print(" Successfull Result : ")
+	for result in inf.result_success:
+		print(colors.GREEN + "  " + result + colors.ENDC)
+	input("Continue")
+	menu()
 
-print(" Loaded: " + colors.GREEN + str(len(domainlist)) + colors.ENDC + " Total of Unique Host: " + str(len(parseddom)) + " host")
-print("")
-input(colors.GREEN + "[ENTER] Start Scan ....." + colors.ENDC)
-print("")
+def menu():
+	print('''
+	
+	__  _  ________ ____   ____  
+	\ \/ \/ /  ___// __ \_/ __ \ 
+	 \     /\___ \\  ___/\  ___/ 
+	  \/\_//____  >\___  >\___  >
+	            \/     \/     \/                       
+	                               
+	''')
+	print("    [" + colors.RED_BG + " Domain : Fronting " + colors.ENDC + "]")
+	print("     ["+colors.RED_BG+" Author " + colors.ENDC + ":" + colors.GREEN_BG + " Kiynox " + colors.ENDC + "]")
+	print("")
+	
+	print(" 1. Scanning File List from .txt")
+	print(" 2. Scanning File List from .csv")
+	print(" 3. Scanning from Subdomain Enum [HackerTarget]")
+	print(" 4. Local Websocket Finder")
+	print(" Q to Quit")
+	opsi=input(" Choose Option :  ")
+	if str(opsi)=="1":
+		text()
+	elif str(opsi)=="2":
+		csv()
+	elif str(opsi)=="3":
+		enum()
+	elif str(opsi)=="4":
+		wsocket()
+	else:
+		exit()
 
-for domain in domainlist:
-		try:
-			r = requests.get("http://" + domain, headers=headers, timeout=0.7, allow_redirects=False)
-			if r.status_code == expected_response:
-				print(" ["+colors.GREEN_BG+" HIT "+colors.ENDC+"] " + domain)
-				print(domain, file=open("RelateCFront.txt", "a"))
-				result_success.append(str(domain))
-			elif r.status_code != expected_response:
-				print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" +colors.RED_BG+" " + str(r.status_code) + " "+colors.ENDC+"]")
-		except (Timeout, ReadTimeout, ConnectionError):
-			print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" + colors.RED_BG +" TIMEOUT "+colors.ENDC+"]")
-		except(ChunkedEncodingError, ProtocolError, InvalidChunkLength):
-			print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" + colors.RED_BG+" Invalid Length "+colors.ENDC + "]")
-			pass
-		except(TooManyRedirects):
-			print(" ["+colors.RED_BG+" FAIL "+colors.ENDC+"] " + domain + " [" +colors.RED_BG+" Redirects Loop "+colors.ENDC+"]")
-		except:
-			pass
-
-print(" Loaded : "  + colors.GREEN + str(len(result_success)) + colors.ENDC)
-if len(result_success) >= 0:
-	print(" Successfull Result : ")
-for result in result_success:
-	print(colors.GREEN + "  " + result + colors.ENDC)
+os.chdir(dirname(abspath(__file__)))
+if not os.path.exists(inf.hostpath):
+	os.makedirs(inf.hostpath)
+menu()
