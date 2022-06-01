aabeb0a0682226b2d05f9cb082990de81ff28fe1
EMSI <62210133+MC874@users.noreply.github.com>
2022-06-01 18:05:16 +0700

    chore: Custom
    - Fixed Custom Domain Fronting from UserInput

--- wseex.py
+++ wseex.py
@@ -81,6 +81,7 @@ __  _  ________ ____   ____
 				print("2. Leave it as default")
 				print("")
 				ansi=input(" Choose Option : ")
+				print("")
 				if str(ansi)=="1":
 					print("")
 					domain=input(" Domain : ")
@@ -91,7 +92,6 @@ __  _  ________ ____   ____
 				else:
 					pass
 
-				print(" Check " + f"{payloads}")
 				headers = payloads
 				num_file=1
 				files = os.listdir(hostpath)
@@ -158,15 +158,16 @@ __  _  ________ ____   ____
 		elif str(opsi)=="2":
 			def csv():
 				global domainlist, headers, frontdom, control_domain
-				
 				print("1. Insert custom fronting domain")
 				print("2. Leave it as default")
 				print("")
 				ansi=input(" Choose Option : ")
+				print("")
 				if str(ansi)=="1":
 					print("")
-					control_domain=input(" Domain : ")
-					print(colors.GREEN_BG + " " + f"{control_domain}" + " " + colors.ENDC + " Selected as Domain Fronting!")
+					domain=input(" Domain : ")
+					payloads["Host"]=f"{domain}"
+					print(colors.GREEN_BG + " " + f"{domain}" + " " + colors.ENDC + " Selected as Domain Fronting!")
 					print(colors.RED_BG+" Warning " + colors.ENDC + " : " + colors.RED_BG + " INVALID " + colors.ENDC + " Domain Will Give 0 Result!" )
 					print("")
 				else:
@@ -240,15 +241,16 @@ __  _  ________ ____   ____
 		elif str(opsi)=="3":
 			def enum():
 				global control_domain
-				
 				print("1. Insert custom fronting domain")
 				print("2. Leave it as default")
 				print("")
 				ansi=input(" Choose Option : ")
+				print("")
 				if str(ansi)=="1":
 					print("")
-					control_domain=input(" Domain : ")
-					print(colors.GREEN_BG + " " + f"{control_domain}" + " " + colors.ENDC + " Selected as Domain Fronting!")
+					domain=input(" Domain : ")
+					payloads["Host"]=f"{domain}"
+					print(colors.GREEN_BG + " " + f"{domain}" + " " + colors.ENDC + " Selected as Domain Fronting!")
 					print(colors.RED_BG+" Warning " + colors.ENDC + " : " + colors.RED_BG + " INVALID " + colors.ENDC + " Domain Will Give 0 Result!" )
 					print("")
 				else:
@@ -299,19 +301,6 @@ __  _  ________ ____   ____
 	elif str(ans)=="2":
 		def wsocket():
 			global domainlist, headers, frontdom, control_domain
-			print("1. Insert custom fronting domain")
-			print("2. Leave it as default")
-			print("")
-			ansi=input(" Choose Option : ")
-			if str(ansi)=="1":
-				print("")
-				control_domain=input(" Domain : ")
-				print(colors.GREEN_BG + " " + f"{control_domain}" + " " + colors.ENDC + " Selected as Domain Fronting!")
-				print(colors.RED_BG+" Warning " + colors.ENDC + " : " + colors.RED_BG + " INVALID " + colors.ENDC + " Domain Will Give 0 Result!" )
-				print("")
-			else:
-				pass
-
 			wsocket = { "Connection": "Upgrade", "Sec-Websocket-Key": "dXP3jD9Ipw0B2EmWrMDTEw==", "Sec-Websocket-Version": "13", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36", "Upgrade": "websocket" }
 			headers = wsocket
 			num_file=1
